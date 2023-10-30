a78db188b8ec58e21d2666778f61b25a0130f043
Joe Comiskey <joecomiskey_asset@outlook.com>
2023-10-29 21:03:16 -0400

    Add docstring

--- app/src/main/java/com/example/guarden/MainActivity.java
+++ app/src/main/java/com/example/guarden/MainActivity.java
@@ -15,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
+
     public void startGame(View view) {
+        /**
+         * Initializes and starts a new game by setting the current content view to a new instance of GameView
+         * @param view: The View object that triggered this method call
+         */
         GameView gameView = new GameView(this);
         setContentView(gameView);
     }
