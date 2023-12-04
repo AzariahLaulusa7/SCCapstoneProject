08a7c74e5c4bfb60b31c0f7b8277ad05a6e47d83
luedicka@email.sc.edu <adrienne.s.luedicke@gmail.com>
2023-12-03 19:39:16 -0500

    Added simple version of main move page (still working on resolving account issue)

---
+++ app/src/main/java/com/example/guarden/MoveMain.java
@@ -0,0 +1,10 @@
+package com.example.guarden;
+
+import android.content.Context;
+import android.view.View;
+
+public class MoveMain extends View {
+    public MoveMain(Context context) {
+        super(context);
+    }
+}
