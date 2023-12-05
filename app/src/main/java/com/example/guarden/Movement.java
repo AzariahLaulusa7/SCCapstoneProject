1d0855585b672292854ef05c96198eacab1d2ca5
ALuedicke <luedicka@email.sc.edu>
2023-12-04 22:49:38 -0500

    Fixed small issues with timer

--- app/src/main/java/com/example/guarden/Movement.java
+++ app/src/main/java/com/example/guarden/Movement.java
@@ -19,8 +19,8 @@ public class Movement extends AppCompatActivity {
     TextView timer;
     Button start;
     ImageView pose;
-    private int poseCount;
     private List<Integer> yogaPoses;
+    private int poseCounter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,23 +28,31 @@ public class Movement extends AppCompatActivity {
         start = (Button) findViewById(R.id.start);
         timer = (TextView) findViewById(R.id.timer);
         pose = (ImageView) findViewById(R.id.pose);
+        poseCounter=0;
+        timer.setVisibility(INVISIBLE);
         yogaPoses = new Poses("yoga");
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                start.setVisibility(INVISIBLE);
-                new CountDownTimer(4*1000, 1000) {
+            start.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    timer.setVisibility(VISIBLE);
+                    start.setVisibility(INVISIBLE);
+                    start.setText("Start");
+                    new CountDownTimer(4 * 1000, 1000) {
 
-                    public void onTick(long millisUntilFinished) {
-                        timer.setText(""+millisUntilFinished/1000);
-                    }
-                    public void onFinish() {
-                        pose.setImageResource(yogaPoses.get(poseCount));
-                        start.setText("Continue");
-                        start.setVisibility(VISIBLE);
-                    }
-                }.start();
-            }
-        });
+                        public void onTick(long millisUntilFinished) {
+                            timer.setText("" + millisUntilFinished / 1000);
+                        }
+
+                        public void onFinish() {
+                            pose.setImageResource(yogaPoses.get(poseCounter));
+                            if(poseCounter==yogaPoses.size()-1) poseCounter=0;
+                            else poseCounter++;
+                            timer.setVisibility(INVISIBLE);
+                            start.setVisibility(VISIBLE);
+
+                        }
+                    }.start();
+                }
+            });
     }
 }
