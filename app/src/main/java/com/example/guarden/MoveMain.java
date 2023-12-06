b4d73cf815764d4b9e851db7914258a8ccdf2224
ALuedicke <luedicka@email.sc.edu>
2023-12-06 15:06:45 -0500

    Added exercises and the ability to toggle between the modes.

--- app/src/main/java/com/example/guarden/MoveMain.java
+++ app/src/main/java/com/example/guarden/MoveMain.java
@@ -1,20 +1,36 @@
 package com.example.guarden;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.content.Intent;
+import android.widget.Button;
 
 public class MoveMain extends AppCompatActivity {
+    Button yoga;
+    Button exercise;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.move_main);
+        yoga = (Button) findViewById(R.id.yoga);
+        exercise = (Button) findViewById(R.id.exercise);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    }
-
-    public void startMovement(View view) {
-        startActivity(new Intent(MoveMain.this, Movement.class));
+        Intent myIntent = new Intent(this, Movement.class);
+        yoga.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                myIntent.putExtra("mode","yoga");
+                startActivity(myIntent);
+            }
+        });
+        exercise.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                myIntent.putExtra("mode","exercise");
+                startActivity(myIntent);
+            }
+        });
     }
 }
+
