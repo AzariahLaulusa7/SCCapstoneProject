75dfae251c5eef00e31474a8823c8569d74587f8
josephbrancker <josephbrancker@email.sc.edu>
2023-12-07 01:27:22 -0500

    Finished Home Screen

--- app/src/main/java/com/example/guarden/HomeScreen.java
+++ app/src/main/java/com/example/guarden/HomeScreen.java
@@ -1,26 +1,117 @@
 package com.example.guarden;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeScreen extends AppCompatActivity {
+
+
+    ImageButton move;
+    ImageButton journal;
+    ImageButton games;
+    ImageButton breath;
+    ImageButton forums;
+    ImageButton add;
+
+    ImageButton profile;
+
+    ImageButton call;
+
+    ImageButton settings;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_home);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        move = (ImageButton) findViewById(R.id.Move);
+        journal = (ImageButton) findViewById(R.id.Journal);
+        games = (ImageButton) findViewById(R.id.Games);
+        breath = (ImageButton) findViewById(R.id.Breath);
+        forums = (ImageButton) findViewById(R.id.Forums);
+        add = (ImageButton) findViewById(R.id.Add);
+        profile = (ImageButton) findViewById(R.id.Profile);
+        call = (ImageButton) findViewById(R.id.Call);
+        settings = (ImageButton) findViewById(R.id.Settings);
+        Intent MoveMain = new Intent(this, MoveMain.class);
+        //Intent name_of_journal_class = new Intent(this, name_of_journal_class.class);
+        //Intent name_of_games_class = new Intent(this, name_of_games_class.class);
+        //Intent name_of_breath_class = new Intent(this, name_of_breath_class.class);
+        //Intent name_of_forums_class = new Intent(this, name_of_forums_class.class);
+        //Intent name_of_add_class = new Intent(this, name_of_add_class.class);
+        //Intent name_of_profile_class = new Intent(this, name_of_profile_class.class);
+        //Intent name_of_call_class = new Intent(this, name_of_call_class.class);
+        //Intent name_of_settings_class = new Intent(this, name_of_settings_class.class);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(MoveMain);
+            }
+        });
+
+        /*journal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_journal_class);
+            }
+        });
+
+        games.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_games_class);
+            }
+        });
+
+        breath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_breath_class);
+            }
+        });
+
+        forums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_forums_class);
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_add_class);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(MoveMain);
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_call_class);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(name_of_settings_class);
+            }
+        });*/
     }
 
-    public void startMove(View view) {
-        /**
-         * Initializes and starts a new game by setting the current content view to a new instance of GameView
-         * @param view: The View object that triggered this method call
-         */
-        move move2 = new move();
-        setContentView(R.layout.move);
-    }
+
 }
