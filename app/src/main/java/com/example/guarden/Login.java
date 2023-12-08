197142d03f9d80cf4b7a1247a509bc886009b57f
Joe Comiskey <joecomiskey_asset@outlook.com>
2023-12-08 12:01:47 -0500

    fixed bug where app would crash if user clicks login with no credentials in text box

--- app/src/main/java/com/example/guarden/Login.java
+++ app/src/main/java/com/example/guarden/Login.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.Button;
 import android.widget.TextView;
+import android.text.TextUtils;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -36,6 +37,12 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
                 String enteredEmail = emailEditText.getText().toString().trim();
                 String enteredPassword = passwordEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(enteredEmail) || TextUtils.isEmpty(enteredPassword)) {
+                    Toast.makeText(Login.this, "Email and password must not be empty", Toast.LENGTH_SHORT).show();
+                    return; // Exit the OnClickListener if fields are empty
+                }
+
                 String emailKey = enteredEmail.replace(".", ",");
 
                 databaseReference.child(emailKey).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -69,14 +76,15 @@ public class Login extends AppCompatActivity {
     }
 
     private static class User {
-        public String email;
-        public String password;
+        public String email, password, firstName, lastName;
 
         public User() {}
 
-        public User(String email, String password) {
+        public User(String email, String password, String firstName, String lastName) {
             this.email = email;
             this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
         }
     }
 }
