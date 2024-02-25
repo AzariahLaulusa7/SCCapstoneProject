0b61b4e6165846159f48ce91ac41319496f450a0
ALuedicke <luedicka@email.sc.edu>
2024-02-25 13:27:55 -0500

    Added pages to make custom workouts and button to go to list of all workouts

--- app/src/main/java/com/example/guarden/MoveMain.java
+++ app/src/main/java/com/example/guarden/MoveMain.java
@@ -11,6 +11,7 @@ import android.widget.ImageButton;
 public class MoveMain extends AppCompatActivity {
     Button yoga;
     Button exercise;
+    Button viewAll;
     ImageButton back;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +19,11 @@ public class MoveMain extends AppCompatActivity {
         yoga = (Button) findViewById(R.id.yoga);
         exercise = (Button) findViewById(R.id.exercise);
         back = (ImageButton) findViewById(R.id.move_back);
+        viewAll = (Button) findViewById(R.id.custom);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         Intent myIntent = new Intent(this, Movement.class);
         Intent myIntent2 = new Intent(this, HomeScreen.class);
+        Intent viewMoveList = new Intent(this, MovementViewAll.class);
         yoga.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -33,6 +36,12 @@ public class MoveMain extends AppCompatActivity {
             public void onClick(View v) {
                 myIntent.putExtra("mode","exercise");
                 startActivity(myIntent);
+            }
+        });
+        viewAll.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(viewMoveList);
             }
         });
         back.setOnClickListener(new View.OnClickListener(){
---
+++ app/src/main/java/com/example/guarden/MovementViewAll.java
@@ -0,0 +1,15 @@
+package com.example.guarden;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.ImageButton;
+public class MovementViewAll extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movement_view_all);
+    }
+}
