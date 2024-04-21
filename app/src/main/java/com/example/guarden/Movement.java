8add28e55d4dd45409a4d0b5b1d57309203a4879
ALuedicke <luedicka@email.sc.edu>
2024-04-21 17:36:58 -0400

    fixed toggle bug, also removed counter update toast message

--- app/src/main/java/com/example/guarden/Movement.java
+++ app/src/main/java/com/example/guarden/Movement.java
@@ -119,7 +119,7 @@ public class Movement extends AppCompatActivity {
                     }
                     if (SaveUser.getThumbsDown(Movement.this) == true) {
                         listCounter--;
-                        Toast.makeText(Movement.this, "Counter: **"+poseCounter+"**", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(Movement.this, "Counter: **"+poseCounter+"**", Toast.LENGTH_LONG).show();
                         SaveUser.setThumbsDown(Movement.this, false);
                     }
                     if (poseList.get(poseCounter).getLike() == 2 && poseList.get(poseCounter).getCategory().equalsIgnoreCase(mode) && listCounter < 0) {
