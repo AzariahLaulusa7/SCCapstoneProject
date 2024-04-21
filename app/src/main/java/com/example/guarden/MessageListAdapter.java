2edb607c1fd9c232b92722d1f7808ccec02b8fe5
james-thurlow <70419678+james-thurlow@users.noreply.github.com>
2024-04-21 15:51:30 -0400

    resolved #163

--- app/src/main/java/com/example/guarden/MessageListAdapter.java
+++ app/src/main/java/com/example/guarden/MessageListAdapter.java
@@ -8,8 +8,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
+import static com.example.guarden.Message.LayoutOne;
+import static com.example.guarden.Message.LayoutTwo;
 
-public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.ViewHolder> {
+public class MessageListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private ArrayList<Message> messages;
     private Context context;
@@ -19,17 +21,38 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
         this.context = context;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messages.get(position);
+        return message.getViewType();
+    }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bot_message, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case Message.LayoutOne:
+                View layoutOne = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_message, parent, false);
+                return new LayoutOneViewHolder(layoutOne);
+            case Message.LayoutTwo:
+                View layoutTwo = LayoutInflater.from(parent.getContext()).inflate(R.layout.bot_message, parent, false);
+                return new LayoutTwoViewHolder(layoutTwo);
+            default:
+                throw new IllegalArgumentException("Invalid view type");
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Message message = messages.get(position);
-        holder.message.setText(message.getMessage());
+        switch (holder.getItemViewType()) {
+            case Message.LayoutOne:
+                ((LayoutOneViewHolder) holder).setMessage(message.getMessage());
+                break;
+            case Message.LayoutTwo:
+                ((LayoutTwoViewHolder) holder).setMessage(message.getMessage());
+                break;
+        }
     }
 
     @Override
@@ -37,14 +60,29 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
         return messages.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView message;
+    static class LayoutOneViewHolder extends RecyclerView.ViewHolder {
+        TextView message;
 
-        public ViewHolder(View itemView) {
+        LayoutOneViewHolder(View itemView) {
             super(itemView);
+            message = itemView.findViewById(R.id.userMessage);
+        }
 
-            message = itemView.findViewById(R.id.tvSpecialty);
+        void setMessage(String text) {
+            message.setText(text);
+        }
+    }
 
+    static class LayoutTwoViewHolder extends RecyclerView.ViewHolder {
+        TextView message;
+
+        LayoutTwoViewHolder(View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.botMessage);
+        }
+
+        void setMessage(String text) {
+            message.setText(text);
         }
     }
 }
